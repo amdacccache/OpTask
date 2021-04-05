543e7d7c632917b639b5f7548e16eac03f7a50cc
Nabil Arbouz <nabilarbouz33@gmail.com>
2021-04-04 19:35:13 -0700

    formated footer component

--- frontend/src/Footer/Footer.js
+++ frontend/src/Footer/Footer.js
@@ -1,39 +1,39 @@
 import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer className="text-center text-lg-start">
-        <div className="p-4">
-          <div className="row align-items-center">
-            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-              <h5 className="text-uppercase">OpTask</h5>
-              <p>© 2021 Copyright: Nabil Arbouz and Anna Daccache</p>
-            </div>
-            <div className="col-lg3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a
-                    href="https://johnguerra.co/classes/webDevelopment_spring_2021/"
-                    className="text-dark"
-                  >
-                    Course Website
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/amdacccache/novelish.io"
-                    className="text-dark"
-                  >
-                    Github
-                  </a>
-                </li>
-              </ul>
-            </div>
+  return (
+    <footer className="text-center text-lg-start">
+      <div className="p-4">
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 className="text-uppercase">OpTask</h5>
+            <p>© 2021 Copyright: Nabil Arbouz and Anna Daccache</p>
+          </div>
+          <div className="col-lg3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a
+                  href="https://johnguerra.co/classes/webDevelopment_spring_2021/"
+                  className="text-dark"
+                >
+                  Course Website
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/amdacccache/novelish.io"
+                  className="text-dark"
+                >
+                  Github
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </footer>
-    )
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
